26f8b533efb26f8dd6a6232d6dcda7c8884e5ee8
cloudq9team2 <cloudq9team2@gmail.com>
2014-06-12 13:55:14 +0900

    browse3

--- actions.php
+++ actions.php
@@ -1,5 +1,5 @@
 <?php
-$m = new MongoClient( 'mongodb://localhost:12345');
+$m = new MongoClient( '131.206.29.116:30000');
 $cursor = $m->tem->users->find( array( 'name' => 'marat'));
 $result = $cursor->getNext();
 echo json_encode( $result); 
